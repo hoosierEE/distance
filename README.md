91659c5aad67d35dbd853163e9bb344c12c7f54e
hoosierEE <ashroyer@gmail.com>
2015-03-03 22:37:54 -0500

    rename html and expand readme

--- README.md
+++ README.md
@@ -4,4 +4,22 @@ Got a hunger for knowledge?  So does [#iubrain](https://twitter.com/iubrain).
 
 Example App
 -----------
-`index.html` uses `app.js` and HTML5's geolocation API to find the user's latitude and longitude.  It then does some math to find how far the user is from #iubrain.
+`app.html` uses `app.js` and HTML5's geolocation API to find the user's latitude and longitude, and calculate the distance between you and the #iubrain.
+
+Brainstorming Additional Functionality
+--------------------------------------
+This is a *minimally* functional example.  But in theory it would be easy to add:
+
+* "Achievements" i.e. touched the brain (dist < 1m)
+* opt-in publish data (anonymously or not, to IU or worldwide, etc.)
+* opt-in link with social media accounts
+* unit conversions meters to light-years, neuron length, alpha wave length, ... (could go crazy with this)
+* ???
+* Profit!
+
+Other
+-----
+
+1. If you make something non-trivial in JavaScript you're gonna have a Bad Time.  Choose something better (React.js or Elm) if we decide to get serious.
+2. Make it beautiful (by today's standards).
+3. Other possible targets: Chrome App, Native App (iOS, Android, Windows Phone)
--- app.js
+++ app.js
@@ -1,35 +1,50 @@
+/* Basre-Bones App */
+/////////////////////
+
 function getLoc() {
+    // TODO: error handling
     navigator.geolocation.getCurrentPosition(howFar);
 }
 
 function howFar(pos) {
     var lat = pos.coords.latitude;
     var lon = pos.coords.longitude;
-    // document.getElementById('lat').innerHTML = lat;
-    // document.getElementById('lon').innerHTML = lon;
     document.getElementById('dfb').innerHTML = getDistance(lat,lon).toFixed(4) + ' km';
 }
 
-//function getDistance(lat2,lon2) {
 function getDistance(rawLat,rawLon) {
-    // Brain coords;
-    // 39.171989, -86.520674
-    // get great circle distance
+    // return great circle distance, in kilometers
     meanEarthRadius = 6371;
     var d;
-    function toRad(deg) {
-        return deg * Math.PI / 180;
-    }
-
+    function toRad(deg) { return deg*Math.PI/180; }
     with(Math) {
         var lat2 = toRad(rawLat);
         var lon2 = toRad(rawLon);
+        // the brain coordinates: 39.171989, -86.520674
         var lat1 = toRad(39.171989);
         var lon1 = toRad(-86.520674);
-        var dlat = abs(lat2 - lat1);
-        var dlon = abs(lon2 - lon1);
+        var dlat = abs(lat2-lat1);
+        var dlon = abs(lon2-lon1);
         // https://en.wikipedia.org/wiki/Great-circle_distance
-        d = acos(sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2)*cos(dlon));
+        d = acos(
+                sin(lat1)*sin(lat2)
+                + cos(lat1)*cos(lat2)*cos(dlon));
     }
     return d * meanEarthRadius;
 }
+
+
+/* possible additions */
+////////////////////////
+
+// return true if dist < 1m
+function touchstone(dist) { return false; }
+
+// enable sharing
+function share(level) { return {}; }
+
+// other units
+function otherUnits(dist, unit) { return 0; }
+
+// history (be careful not to scare people)
+function appendToHistory(newDistanceRecording) { return {}; }
