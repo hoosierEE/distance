c57b48db8a4a2b86f1074c36b4e1384eb60b497d
hoosierEE <ashroyer@gmail.com>
2015-03-24 20:58:00 -0400

    fallback to invalid geolocation params to indicate error

--- geo.js
+++ geo.js
@@ -1,8 +1,8 @@
 var g = Elm.fullscreen(Elm.WhereBrain, {geo:{lat:0,lon:0,hdg:0}});
 
 navigator.geolocation.watchPosition(function(p) {
-    var la = p.coords.latitude || 0,
-        lo = p.coords.longitude || 0,
+    var la = p.coords.latitude || 91,
+        lo = p.coords.longitude || 91,
         hd = p.coords.heading || 361;
     g.ports.geo.send({lat:la,lon:lo,hdg:hd});
 });
