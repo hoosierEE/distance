edb2e0e4d16ada27333850cba7c0404457646c4f
hoosierEE <ashroyer@gmail.com>
2015-03-24 21:00:06 -0400

    give invalid default values to latitude, longitude, and heading

--- geo.js
+++ geo.js
@@ -2,7 +2,7 @@ var g = Elm.fullscreen(Elm.WhereBrain, {geo:{lat:0,lon:0,hdg:0}});
 
 navigator.geolocation.watchPosition(function(p) {
     var la = p.coords.latitude || 91,
-        lo = p.coords.longitude || 91,
+        lo = p.coords.longitude || 181,
         hd = p.coords.heading || 361;
     g.ports.geo.send({lat:la,lon:lo,hdg:hd});
 });
